6e1a72cdf6e0d29e2678261695d10b9c7618876f
Aaronleon13 <aaronleon13@gmail.com>
2025-03-31 10:58:36 -0700

    Añade vista de personajes con diseño mejorado y fuente personalizada; elimina vista de inicio.

--- src/Personajes/views/PersonajeView.vue
+++ src/Personajes/views/PersonajeView.vue
@@ -1,9 +1,29 @@
 <template>
   <section>
     <header class="mt-8">
-      <h1 class="text-center text-2xl md:text-4xl font-bold text-amber-500 capitalize">
+      <h1
+        class="text-center text-2xl md:text-4xl font-bold text-amber-500 capitalize font-dbz tracking-wider"
+      >
         dragon ball z - personajes
       </h1>
+      <div class="grid grid-cols-2 md:grid-cols-4 grid-rows-1 max-w-4xl mx-auto mt-12">
+        <div
+          class="flex flex-col items-center justify-center"
+          v-for="personaje in personajes"
+          :key="personaje.id"
+        >
+          <h2 class="text-xl font-bold text-gray-800 font-dbz tracking-wider">
+            {{ personaje.name }}
+          </h2>
+          <div class="flex flex-col items-center justify-center">
+            <img
+              :src="personaje.image"
+              :alt="personaje.name"
+              class="size-80 object-contain mb-4 drop-shadow-lg hover:scale-105 transition-all duration-300 cursor-pointer hover:skew-30 hover:rotate-3"
+            />
+          </div>
+        </div>
+      </div>
     </header>
   </section>
 </template>
--- src/views/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <main>
-    <h1 class="text-center text-cyan-300 text-3xl md:text-5xl">HOME</h1>
-  </main>
-</template>

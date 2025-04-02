30ad1f2c6d6e28978e34a0d4c11c05e34a489947
Aaronleon13 <aaronleon13@gmail.com>
2025-04-02 10:37:16 -0700

    Refactor la vista de personajes: extrae componentes HeaderComp y personajesComp para mejorar la estructura del código.

---
+++ src/Personajes/componets/headerComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="mt-8">
+    <h1
+      class="text-center text-2xl md:text-4xl font-bold text-amber-600 capitalize font-dbz tracking-wider"
+    >
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: 'Dragon Ball Z - Personajes',
+  },
+})
+</script>
---
+++ src/Personajes/componets/personajesComp.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="grid grid-cols-2 md:grid-cols-4 grid-rows-1 max-w-4xl mx-auto mt-12 gap-10">
+    <div
+      class="flex flex-col items-center justify-center rounded-lg bg-gradient-to-b from-amber-300 to-amber-600 p-4 shadow-lg hover:shadow-2xl transition-all duration-300 cursor-pointer"
+      v-for="personaje in personajes"
+      :key="personaje.id"
+    >
+      <h2 class="text-4xl font-bold text-amber-600 font-dbz tracking-widest">
+        {{ personaje.name }}
+      </h2>
+      <div class="flex flex-col items-center justify-center">
+        <img
+          :src="personaje.image"
+          :alt="personaje.name"
+          class="size-80 object-contain mb-4 hover:drop-shadow-3xl hover:scale-125 transition-all duration-300 cursor-pointer"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const personajes = ref([])
+const links = ref([])
+
+const fetchData = async () => {
+  const response = await fetch('https://dragonball-api.com/api/characters')
+  const data = await response.json()
+  personajes.value = data.items
+  links.value = data.links
+
+  console.log(links.value)
+  console.log(personajes.value)
+}
+
+onMounted(() => {
+  fetchData()
+})
+</script>

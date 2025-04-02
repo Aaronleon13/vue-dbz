a12c9c3eaf043e2cfa71a5d4bd0e31ecf4c1080c
Aaronleon13 <aaronleon13@gmail.com>
2025-04-02 10:40:38 -0700

    Elimina el script vacío de App.vue y limpia el código de estilo innecesario en PersonajeView.vue.

--- src/Personajes/views/PersonajeView.vue
+++ src/Personajes/views/PersonajeView.vue
@@ -9,16 +9,3 @@
 import HeaderComp from '../componets/headerComp.vue'
 import personajesComp from '../componets/personajesComp.vue'
 </script>
-
-<style>
-.ts {
-  text-shadow:
-    0 0 5px #000,
-    0 0 10px #000,
-    0 0 15px #000,
-    0 0 20px #000,
-    0 0 25px #000,
-    0 0 30px #000,
-    0 0 35px #000;
-}
-</style>
